42ceaf5b44489902b161c27e20b6e21a25be54e1
Ivancaas <ivancaaas@gmail.com>
2022-01-22 09:51:18 +0100

    Initial Commit

---
+++ clientTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  clientTest
+//
+//  Created by Ivan on 22/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ clientTest/clientTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  clientTestApp.swift
+//  clientTest
+//
+//  Created by Ivan on 22/1/22.
+//
+
+import SwiftUI
+
+@main
+struct clientTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
